ef9fee5e33792791ecd6314773bdf04ff0ad2543
Mykhailo Bubnov <applemix00@gmail.com>
2023-01-08 16:06:06 +0100

    UserService updated to work with dao entity

--- src/main/java/com/ourgame/ourgameserver/ws/model/user/UserService.java
+++ src/main/java/com/ourgame/ourgameserver/ws/model/user/UserService.java
@@ -1,20 +1,31 @@
 package com.ourgame.ourgameserver.ws.model.user;
 
+import com.ourgame.ourgameserver.ws.controllers.dto.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
-    public UserJpaEntity saveUser(UserJpaEntity userJpaEntity) {
-        return userRepository.save(userJpaEntity);
+    public UserJpaEntity saveUser(User userDto) {
+        UserJpaEntity user = new UserJpaEntity();
+        user.setId(userDto.getUsername());
+        user.setPassword(encoder.encode(userDto.getPassword()));
+        user.setEmail(userDto.getEmail());
+        user.setAuthorities(userDto.getAuthorities());
+        System.out.println(user.getId() + " " + user.getPassword() + " " + user.getEmail());
+        return userRepository.save(user);
     }
 
 
