4c65cdd3642f935e0a2c0b871d73ba99f7be52c5
Mykhailo Bubnov <applemix00@gmail.com>
2023-01-08 16:13:28 +0100

    Database user service with password encoder organized

--- src/main/java/com/ourgame/ourgameserver/ws/controllers/UsersController.java
+++ src/main/java/com/ourgame/ourgameserver/ws/controllers/UsersController.java
@@ -1,12 +1,16 @@
 package com.ourgame.ourgameserver.ws.controllers;
 
+import com.ourgame.ourgameserver.ws.controllers.dto.User;
 import com.ourgame.ourgameserver.ws.model.user.UserService;
 import com.ourgame.ourgameserver.ws.security.service.TokenService;
 import com.ourgame.ourgameserver.ws.controllers.dto.StringPlusImage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,8 +20,10 @@ public class UsersController {
     private static final Logger LOG = LoggerFactory.getLogger(UsersController.class);
     private final TokenService tokenService;
     private final UserService userService;
+    private PasswordEncoder encoder;
 
     public UsersController(TokenService tokenService, UserService userService) {
+        userService.saveUser(new User("user",  "password", new SimpleGrantedAuthority("ROLE_USER")));
         this.tokenService = tokenService;
         this.userService = userService;
 
@@ -25,7 +31,7 @@ public class UsersController {
 
     @CrossOrigin
     @GetMapping("/token")
-    public String token(Authentication authentication) {
+    public String getToken(Authentication authentication) {
         LOG.info("token requested for user: '{}'", authentication.getName());
         String token = tokenService.generateToken(authentication);
         LOG.debug("token generated: '{}'", token);
@@ -39,8 +45,8 @@ public class UsersController {
      * @return always 200 code + test string
      */
     @CrossOrigin
-    @GetMapping("/test")
-    public ResponseEntity<StringPlusImage> test(Authentication authentication) {
+    @GetMapping("/stats")
+    public ResponseEntity<StringPlusImage> displayStats(Authentication authentication) {
         return ResponseEntity.ok(new StringPlusImage(authentication.getName(), "https://lh3.googleusercontent.com/aPeSCag8eHV8Xfsu2FdRzRrV0KzD3CWkO8jGbvGZSFIvA_5-8BJ6cHh0lkvqXeUYFwDRp03pH3HdqMNv9--Pv_jw0z1USaKyjg=s400"));
     }
 
